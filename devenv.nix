969fe75417dbaab9c0a6afef3d1b60b8907e74de
Pat Chormai <pat.chormai@gmail.com>
2025-07-29 10:12:51 +0200

    devenv: update

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,13 @@
     poetry.enable = true;
     poetry.activate.enable = true;
   };
+
+  git-hooks.hooks.check-case-conflicts.enable = true;
+  git-hooks.hooks.check-merge-conflicts.enable = true;
+  git-hooks.hooks.check-toml.enable = true;
+  git-hooks.hooks.check-yaml.enable = true;
+  git-hooks.hooks.trim-trailing-whitespace.enable = true;
+
   git-hooks.hooks.ruff.enable = true;
   git-hooks.hooks.ruff-format.enable = true;
 }
